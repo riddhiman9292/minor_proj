94dc591e8984c811277fee0c525f6d8e938e36d2
Riddhiman <shubro17@outlook.com>
2023-11-12 17:03:00 +0530

    Added location services

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const http = require('http');
 app.set('view engine','ejs');
+app.enable('trust proxy');
 app.use(cors());
 app.get('/sample',(req,res)=>{
     console.log(req.socket.remoteAddress);
