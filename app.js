dc55073eb8823c4b1f0f54d5619726992a8ae994
Riddhiman <shubro17@outlook.com>
2023-11-13 03:03:13 +0530

    Views added

--- app.js
+++ app.js
@@ -6,9 +6,14 @@ app.set('view engine','ejs');
 app.enable('trust proxy');
 app.use(cors());
 app.get('/sample',async (req,res)=>{
-    console.log(req.ip);
-    res.send('ok');
+    res.status(200).send({
+        Message:`ok`,
+        ip:`${req.ip}`
+    })
 });
+app.get('/home',async (req,res)=>{
+    res.render('tenant_view');
+})
 const server = http.createServer(app);
 server.listen(3000,()=>{
     console.log('Listening');
