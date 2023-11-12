34757751f9428337cd8d67d70ecb9e53e5aff97f
Riddhiman <shubro17@outlook.com>
2023-11-12 16:42:52 +0530

    location

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const http = require('http');
+app.set('view engine','ejs');
+app.use(cors());
+app.get('/sample',(req,res)=>{
+    console.log(req.socket.remoteAddress);
+    res.send('ok');
+});
+const server = http.createServer(app);
+server.listen(3000,()=>{
+    console.log('Listening');
+})
