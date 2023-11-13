6d4736a9a73b0ad23bf62c3a767a637a6039443a
Riddhiman <shubro17@outlook.com>
2023-11-14 01:08:29 +0530

    request ips condition mod2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const {verified_ips} = require('./config');
 app.enable('trust proxy');
 app.use(cors());
 app.get('/sample',async (req,res)=>{
-    if(`${req.ip}` in verified_ips){
+    if(verified_ips.find(`${req.ip}`)!=-1){
         res.status(200).send({
             Message:`ok`,
             ip:`${req.ip}`
